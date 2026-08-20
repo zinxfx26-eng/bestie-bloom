acf0aae9d10275c910a7fa478bef9d83f3095829
zinxfx26-eng <zinxfx26@gmail.com>
2026-08-20 16:06:04 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,58 +1,92 @@
 const garden=document.getElementById("garden");
 
-for(let i=0;i<60;i++){
-const s=document.createElement("div");
-s.className="spark";
-s.style.left=Math.random()*100+"vw";
-s.style.top=Math.random()*100+"vh";
-s.style.animationDuration=(4+Math.random()*6)+"s";
-garden.appendChild(s);
+for(let i=0;i<70;i++){
+
+const star=document.createElement("div");
+
+star.className="spark";
+
+star.style.left=Math.random()*100+"vw";
+
+star.style.top=Math.random()*100+"vh";
+
+star.style.animationDuration=(5+Math.random()*8)+"s";
+
+garden.appendChild(star);
+
 }
 
 document.body.addEventListener("click",(e)=>{
 
 const x=e.clientX;
+
 const y=e.clientY;
 
 const stem=document.createElement("div");
+
 stem.className="stem";
+
 stem.style.left=x+"px";
+
 stem.style.bottom=(window.innerHeight-y)+"px";
+
 garden.appendChild(stem);
 
 setTimeout(()=>{
 
-for(let i=0;i<20;i++){
-const p=document.createElement("div");
-p.className="petal";
-p.style.left=(x+40)+"px";
-p.style.top=(y-140)+"px";
-p.style.setProperty("--x",(Math.random()*180-90)+"px");
-p.style.setProperty("--y",(Math.random()*180-90)+"px");
-garden.appendChild(p);
-setTimeout(()=>p.remove(),1000);
+for(let i=0;i<25;i++){
+
+const petal=document.createElement("div");
+
+petal.className="petal";
+
+petal.style.left=(x+40)+"px";
+
+petal.style.top=(y-140)+"px";
+
+petal.style.setProperty("--x",(Math.random()*200-100)+"px");
+
+petal.style.setProperty("--y",(Math.random()*200-100)+"px");
+
+garden.appendChild(petal);
+
+setTimeout(()=>petal.remove(),1000);
+
 }
 
 const photo=document.createElement("div");
+
 photo.className="photo";
-photo.style.left=(x-42)+"px";
-photo.style.top=(y-185)+"px";
-photo.innerHTML='<img src="assets/bestie.png">';
+
+photo.style.left=(x-45)+"px";
+
+photo.style.top=(y-190)+"px";
+
+photo.innerHTML=`<img src="assets/bestie.jpg">`;
+
 garden.appendChild(photo);
 
-const l1=document.createElement("div");
-l1.className="leaf";
-l1.style.left=(x-22)+"px";
-l1.style.top=(y-80)+"px";
-l1.style.transform="rotate(-30deg)";
+const leaf1=document.createElement("div");
 
-const l2=document.createElement("div");
-l2.className="leaf";
-l2.style.left=(x+5)+"px";
-l2.style.top=(y-40)+"px";
-l2.style.transform="rotate(140deg)";
+leaf1.className="leaf";
 
-garden.append(l1,l2);
+leaf1.style.left=(x-22)+"px";
+
+leaf1.style.top=(y-85)+"px";
+
+leaf1.style.transform="rotate(-35deg)";
+
+const leaf2=document.createElement("div");
+
+leaf2.className="leaf";
+
+leaf2.style.left=(x+4)+"px";
+
+leaf2.style.top=(y-45)+"px";
+
+leaf2.style.transform="rotate(140deg)";
+
+garden.append(leaf1,leaf2);
 
 },700);
 
